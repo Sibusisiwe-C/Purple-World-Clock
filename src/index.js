d0b00b71647e5db101c4a73f63505a11cea9ffe1
Sibusisiwe-C <sibusisiwenompumelelo202@gmail.com>
2024-07-14 02:02:21 +0200

    added "my location" option

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   if (cityTimeZone === "") {
     return;
   }
